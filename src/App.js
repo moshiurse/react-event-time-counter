c50cde4316cc41ce3d1d88c21b3fa7fd2276af9b
Moshiur <rmoshiur705@gmail.com>
2020-01-11 15:08:10 +0600

    initialized with Event Input container

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import EventImput from './components/EventInput/EventInput';
+
+function App() {
+  return (
+    <div className="App">
+      <EventImput />
+    </div>
+  );
+}
+
+export default App;
