86c580ec5818a1b802fd32b3e78f0e57c72394d1
Moshiur Rahman <rmoshiur705@gmail.com>
2020-01-15 00:42:39 +0600

    added data dynamically and clear input fiels after added data

--- src/App.js
+++ src/App.js
@@ -1,59 +1,59 @@
-import React from 'react';
-import './App.css';
-import EventImput from './components/EventInput/EventInput';
-import EventList from './components/EventList/EventList';
+import React from "react";
+import "./App.css";
+import EventInput from "./components/EventInput/EventInput";
+import EventList from "./components/EventList/EventList";
 
 class App extends React.Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
+    this.clickhandler = this.clickhandler.bind(this);
+
     this.state = {
       items: [
-        {title: 'Go to Mymensingh',
-        date: '2020-01-01',
-        day: 'Saturday',
-        remaining: '14 Days 5 hours 10 min'},
-        {title: 'Go to Mymensingh',
-        date: '2020-01-01',
-        day: 'Saturday',
-        remaining: '14 Days 5 hours 10 min'},
-        {title: 'Go to Mymensingh',
-        date: '2020-01-01',
-        day: 'Saturday',
-        remaining: '14 Days 5 hours 10 min'},
-        {title: 'Go to Mymensingh',
-        date: '2020-01-01',
-        day: 'Saturday',
-        remaining: '14 Days 5 hours 10 min'},
-        {title: 'Go to Mymensingh',
-        date: '2020-01-01',
-        day: 'Saturday',
-        remaining: '14 Days 5 hours 10 min'}
+        {
+          title: "Go to Mymensingh",
+          date: "2020-01-01",
+          day: "Saturday",
+          remaining: "14 Days 5 hours 10 min"
+        }
       ]
-    }
+    };
   }
-  
-  render(){
-        
+
+  clickhandler(eventTitle, eventDate) {
+    var data = {
+      title: eventTitle,
+      date: eventDate,
+      day: "Saturday",
+      remaining: "1 Days 5 hours 10 min"
+    };
+
+    this.state.items.push(data);
+
+    this.setState({ items: this.state.items });
+    console.log(this.state.items);
+  }
+
+  render() {
     return (
       <div className="App">
-  
-      <div className="event-input-container">
-        <EventImput />
-      </div>
-      <div className="event-list-container">
-        {this.state.items.map((item, index) => 
-          
-            <EventList key={index} title={item.title} date={item.date} day={item.day} remaining={item.remaining}/>
-          
-        )}
-      </div>
-        
+        <div className="event-input-container">
+          <EventInput clicked={this.clickhandler} />
+        </div>
+        <div className="event-list-container">
+          {this.state.items.map((item, index) => (
+            <EventList
+              key={index}
+              title={item.title}
+              date={item.date}
+              day={item.day}
+              remaining={item.remaining}
+            />
+          ))}
+        </div>
       </div>
     );
-
   }
-  
 }
 
 export default App;
--- src/components/EventInput/EventInput.js
+++ src/components/EventInput/EventInput.js
@@ -1,48 +1,76 @@
-import React from 'react';
+import React from "react";
 
-import '../EventInput/EventInput.css';
+import "../EventInput/EventInput.css";
 
-class EventInput extends React.Component{     
-        
-    constructor(props){
-        super(props);
+class EventInput extends React.Component {
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            term: '',
-            items: []
-          }
-    }
+    this.changeDate = this.changeDate.bind(this);
+    this.changeTitle = this.changeTitle.bind(this);
 
+    this.state = {
+      title: "",
+      date: ""
+    };
+  }
 
-    render(){
+  changeTitle(event) {
+    const title = event.target.value;
+    this.setState({ title: title });
+  }
 
-        const createEvent = (event) => {
-            let title = document.getElementById('title').value;
-            let date = document.getElementById('date').value;
-        }
+  changeDate(event) {
+    const date = event.target.value;
+    this.setState({ date: date });
+  }
 
-        return(
-            <div className="event-input-container">
-                <h2>An Event Time Counter App</h2>
-            <form onSubmit={this.createEvent}>
-                <div className="row">
-                    <div className="input-field col s6">
-                        <input id="title" type="text" className="validate" value={this.state.term}/>
-                        <label for="title">Write your Event Title</label>
-                    </div>
-                <div className="input-field col s6">
-                    <input id="date" type="datetime-local" className="validate"/>
-                    <label for="date"></label>
-                </div>
+  render() {
+    return (
+      <div className="event-input-container">
+        <h3>An Event Time Counter App</h3>
+        <form>
+          <div className="row">
+            <div className="input-field col s6">
+              <input
+                id="title"
+                type="text"
+                className="validate"
+                onChange={this.changeTitle}
+                value={this.state.title}
+              />
+              <label htmlFor="title">Write your Event Title</label>
             </div>
-            <a type="submit" href="#" 
-            id="create-event" 
-            // onClick={createEvent}
-            class="waves-effect waves-light deep-purple lighten-3 btn"><i class="material-icons note_add">add</i></a>
-            </form>
-            </div>  
-        );
-    }
+            <div className="input-field col s6">
+              <input
+                id="date"
+                type="datetime-local"
+                className="validate"
+                onChange={this.changeDate}
+                value={this.state.date}
+              />
+              <label htmlFor="date"></label>
+            </div>
+          </div>
+          <a
+            type="submit"
+            href="#"
+            id="create-event"
+            onClick={() => {
+              this.props.clicked(this.state.title, this.state.date);
+              this.setState({
+                title: "",
+                date: ""
+              });
+            }}
+            class="waves-effect waves-light deep-purple lighten-3 btn"
+          >
+            <i class="material-icons note_add">add</i>
+          </a>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default EventInput;
